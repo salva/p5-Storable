7096232c928197d738a389894661f9a9dfcbc194
Salvador <sfandino@yahoo.com>
2013-06-12 13:36:17 +0200

    include perl version in configure_and_test output file names

--- configure_and_test.sh
+++ configure_and_test.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 mkdir buildlog 2>/dev/null
+version=$( perl -MConfig -e 'print "$Config::Config{version}"' )
 archname=$( perl -MConfig -e 'print "$Config::Config{archname}"' )
-res=$( (perl -V && perl Makefile.PL && make && make test ) 2>&1 ) && echo "$res" >buildlog/${archname}.PASS || echo "$res" >buildlog/${archname}.FAIL
+res=$( (perl -V && perl Makefile.PL && make && make test ) 2>&1 ) && echo "$res" >buildlog/${version}-${archname}.PASS || echo "$res" >buildlog/${version}-${archname}.FAIL
 make distclean >/dev/null 2>&1
