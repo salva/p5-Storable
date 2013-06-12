177a397025326ab0ca4fb9d1c706b1669fc1ff0f
Salvador <sfandino@yahoo.com>
2013-06-12 12:33:18 +0200

    improve perlbrew testing script

--- configure_and_test.sh
+++ configure_and_test.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-perl Makefile.PL
-make
-make test
+mkdir buildlog 2>/dev/null
+archname=$( perl -MConfig -e 'print "$Config::Config{archname}"' )
+res=$( (perl -V && perl Makefile.PL && make && make test ) 2>&1 ) && echo "$res" >buildlog/${archname}.PASS || echo "$res" >buildlog/${archname}.FAIL
 make distclean >/dev/null 2>&1
